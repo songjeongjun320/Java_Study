15c9da8b6366a254c730ab5aedbe3139ef8cb91e
Jun <songjeongjun320@gmail.com>
2024-02-06 18:25:34 -0700

    02/06/2024

--- Driver.java
+++ Driver.java
@@ -1,16 +1,16 @@
 class Driver {
-    public static void main(String[] args) {
-      int a = 80;
-      int b = 0;
-  
-      for (int c = 3; c <= 6; c++) {
-        b = 6;
-        while (b > c) {
-          b = b - 1;
-          a = a - b;
-        }
-        b = b + a;
+  public static void main(String[] args) {
+    int a = 98;
+    int b = 0;
+
+    for (int c = 3; c <= 6; c++) {
+      b = 0;
+      while (b < c) {
+        b = b + 1;
+        a = a - b;
       }
-      System.out.print(b);
+      b = b + a;
     }
+    System.out.print(b);
   }
+}
